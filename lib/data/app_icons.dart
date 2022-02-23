1ac24e6527b762369a3abb22394751c3f2c738db
manassur <ologundemanasseh@gmail.com>
2022-02-23 23:15:18 +0100

    Added method to return country icon from currency code

--- lib/data/app_icons.dart
+++ lib/data/app_icons.dart
@@ -3,6 +3,26 @@ class AppIcons{
   static String convertIcon ="assets/convert_icon.svg";
   static String infoIcon ="assets/info_icon.svg";
 
-
+  // pass in currency codes to this method and it suggests
+  // a icon to be used to represent the currency
+  static String getIconFromCurrencyCode(code){
+    switch(code) {
+      case "USD":
+        return 'us_flag.png';
+      case 'MXN':
+        return 'mxn_flag.png';
+      case 'PLN':
+        return 'pln_flag.png';
+      case 'AUD':
+        return 'aud_flag.png';
+      case 'CAD':
+        return 'cad_flag.png';
+      case 'EUR':
+        return 'eur_flag.png';
+      default: {
+        return '';
+      }
+    }
+  }
 
 }
